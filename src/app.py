c65477aa96be2f51548f1469de9156ccd8046686
Scaffolder <scaffolder@backstage.io>
2025-09-19 01:27:58 +0000

    initial commit

---
+++ src/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/info')
+def info():
+    return jsonify({
+            'time':datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
+            'hostname': socket.gethostname(),
+            'message': 'You are doing grat job, human :)',
+            'deployed_on': 'kubernetes',
+            'env': 'dev',
+            'app_name': 'python-app-2'
+    }) 
+
+@app.route('/api/v1/health')
+def healthz():
+    return jsonify({'status':'up'}), 200
+
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0")
+
+#'/api/v1/details'
+#'/api/v1/health'
